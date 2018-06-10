594df7d481341c4b84bdf335f3ebdaafe393a739
Victor.Morales <Victor.Morales@Morales>
2018-06-10 17:52:14 -0600

    creacion del servicio edificacion

---
+++ src/AbstractFactory/ServicioEdif.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory;
+
+/**
+ *
+ * @author Victor.Morales
+ */
+public interface ServicioEdif {
+    public void CrearGrupo ();
+    public void CrearVehiculos ();
+    public void CrearEdificacion ();
+}
