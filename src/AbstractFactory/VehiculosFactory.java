8e2a6a34991165156ae715c12658baf7bad18fbb
Victor.Morales <Victor.Morales@Morales>
2018-06-10 17:46:47 -0600

    Creacion de metodos y clases

---
+++ src/AbstractFactory/VehiculosFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory;
+
+/**
+ *
+ * @author Victor.Morales
+ */
+public class VehiculosFactory  implements ServicioFactory{
+
+    @Override
+    public ServicioEdif crearServicio() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new Raza2 ();
+    }
+    
+}
