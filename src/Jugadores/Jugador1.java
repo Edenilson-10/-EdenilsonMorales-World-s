f456d490f76b50783cea87b3eaa310ee9a9b813f
Victor.Morales <Victor.Morales@Morales>
2018-06-14 23:16:32 -0600

    Creacion del jugador 1

---
+++ src/Jugadores/Jugador1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jugadores;
+
+import entity.RazaAbstract;
+
+/**
+ *
+ * @author Victor.Morales
+ */
+public class Jugador1 extends Jugadores{
+    
+
+    public Jugador1(String nombre , RazaAbstract raza) {
+        super(nombre, raza);
+    }
+
+    @Override
+    public String getNombre() {
+        return super.getNombre(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public RazaAbstract getRaza() {
+        return super.getRaza(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    
+}
