2ff2b7331498c24c90b51c0afaf68541cec47291
Victor.Morales <Victor.Morales@Morales>
2018-06-14 23:36:02 -0600

    importa servicios

---
+++ src/service/AbstractService.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import entity.AbstractOp;
+import entity.Grupo;
+import entity.ObjectFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author asycuda
+ */
+public abstract class AbstractService {
+//    private static List lstG = new ArrayList<AbstractOp>();
+    
+//    public Object crearServicio(String nombre) {
+//        
+//        Grupo g = ObjectFactory.crearGrupo();
+//        g.setNombre(nombre);
+//        getLstG().add(g);
+//        System.err.println("Tamaño de lista de grupo: "+getLstG().size());
+//        return g;
+////        return ObjectFactory.CrearGrupo();
+//    }
+    
+    /**
+     * @return the g
+     */
+//    public List getLstG() {
+//        return lstG;
+//    }
+//
+//    /**
+//     * @param g the g to set
+//     */
+//    public void setLstG(List lstG) {
+//        this.lstG = lstG;
+//    }
+    
+//    public static List getlistServicio() {
+////        for(AbstractService g : (List<AbstractService>) lstG){
+////            System.err.println(g.getNombre());
+////        }
+//        return lstG;
+//    }
+}
