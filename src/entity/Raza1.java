8f45af45f055a332c18c3a3895de0a0ed9b61e80
Victor.Morales <Victor.Morales@Morales>
2018-06-14 23:30:51 -0600

    genera raza 1

---
+++ src/entity/Raza1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Victor.Morales
+ */
+public class Raza1 extends RazaAbstract{
+
+      
+    
+   
+    
+    
+    
+//    private String nombre;
+//
+//    /**
+//     * @return the nombre
+//     */
+//    public String getNombre() {
+//        return nombre;
+//    }
+//
+//    /**
+//     * @param nombre the nombre to set
+//     */
+//    public void setNombre(String nombre) {
+//        this.nombre = nombre;
+//    }
+//    
+
+    @Override
+    public String getDescripcion() {
+        return super.getDescripcion(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+    
+}
+
