0ed650ea0307581686ac19fef91172194f366e56
Victor.Morales <Victor.Morales@Morales>
2018-06-14 23:24:26 -0600

    genera grupo

---
+++ src/entity/Grupo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Victor.Morales
+ */
+public class Grupo extends AbstractOp {
+    public Grupo() {
+    }
+
+    
+}
