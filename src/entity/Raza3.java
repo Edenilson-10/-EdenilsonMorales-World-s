7035e4cdade91fb8659fc3d11a2cce8bc19b81fc
Victor.Morales <Victor.Morales@Morales>
2018-06-14 23:31:10 -0600

    genera raza 3

---
+++ src/entity/Raza3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Victor.Morales
+ */
+public class Raza3 extends RazaAbstract{
+ @Override
+    public String getDescripcion() {
+        return super.getDescripcion(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
